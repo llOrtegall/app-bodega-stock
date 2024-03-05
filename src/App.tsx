2abfc5866908a7ea51a5885bdbf416a9693e685a
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-03-05 15:00:08 -0500

    agregando la navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import { Routes, Route } from "react-router-dom";
 
 import { AboutPage, HomePage, LoginPage, UserPage, NotFound } from './Pages'
 
+import { NavBar } from "./components/ui";
+
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/user" element={<UserPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/user" element={<UserPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+
   )
 }
---
+++ src/components/ui/NavBar.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+export function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/user">User</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
