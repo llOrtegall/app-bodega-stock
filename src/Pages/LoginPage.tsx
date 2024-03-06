70e4c2cf5ad15b5b27eaa011fd1566cff48af83d
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-03-06 10:39:55 -0500

    agregnado axios al proyecto

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import { Label, Button, Input } from "../components/ui";
 
 export function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (ev: React.FormEvent) => {
+    ev.preventDefault();
+    console.log({ username, password });
+  }
+
+
   return (
     <section className="flex justify-center">
 
-      <form className="flex flex-col w-[480px] gap-2 border p-4 mt-10 rounded-lg bg-gray-200">
-        <Label htmlFor="usuario">Usuario</Label>
-        <Input placeholder="CP141412422" 
-          type="text" id="usuario" required />
+      <form className="flex flex-col w-[480px] gap-2 border p-4 mt-10 rounded-lg bg-gray-200" onSubmit={handleSubmit}>
+        <Label htmlFor="username">Usuario</Label>
+        <Input placeholder="CP141412422" onChange={ ev => setUsername(ev.target.value) }
+          type="text" id="username" required />
 
         <Label htmlFor="password">Contraseña</Label>
-        <Input placeholder="**********" 
+        <Input placeholder="**********" onChange={ ev => setPassword(ev.target.value) }
           type="password" id="password" required />
 
         <Button type="submit">Ingresar</Button>
