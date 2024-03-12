0d67f899ecd81096afa4db254a942030ae88f71c
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-03-12 12:19:21 -0500

    esto funciona extendamos la funcionalidad

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,14 +1,47 @@
-import { NavLink } from "react-router-dom";
+import { useState, useEffect, useRef } from 'react';
+import { useAuth } from "../../Auth/AuthContext";
+import { ButtonDow } from "../icons";
+import { Articulos } from "./NavLinks";
 
 export function NavBar() {
+  const { user } = useAuth();
+  const rol = user?.rol;
+  const [showArticulos, setShowArticulos] = useState(false);
+  const node = useRef<HTMLLIElement | null>(null);
+
+  const handleClickOutside = (e: MouseEvent) => {
+    if (node.current && !node.current.contains(e.target as Node)) {
+      setShowArticulos(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <nav className="flex p-3 bg-slate-900">
+    <nav className="flex p-3 bg-slate-900 justify-around">
       <figure>
         <img src="/gane.png" width={110} alt="Foto Gane" />
       </figure>
-      <ul>
-        <li>
-          <NavLink to={"/home"} className={({ isActive }) => isActive ? 'active' : ''}>Home</NavLink>
+      <ul className="flex items-center gap-4 text-white text-xl">
+        <li className="relative" ref={node}>
+          <button 
+            id='Articulos' 
+            className="flex font-semibold text-white items-center hover:text-blue-400" 
+            onClick={() => setShowArticulos(!showArticulos)}
+          >
+            <span>Artículos</span>
+            <figure className="flex items-center pt-1">
+              <ButtonDow />
+            </figure>
+          </button>
+
+          {showArticulos && <Articulos key={'articulos'} rol={rol}/>}
+
         </li>
       </ul>
     </nav>
--- src/components/ui/NavLinks/Articulos.tsx
+++ src/components/ui/NavLinks/Articulos.tsx
@@ -1,48 +1,43 @@
 import { NavLink } from 'react-router-dom'
-import { SuccesIcon, LockIcon, ButtonDow } from '../../icons'
+import { SuccesIcon, LockIcon } from '../../icons'
 
-export function Articulos ({ handleClickMenu, renderMenu, handleLinkClick, authorize }) {
+interface ArticulosProps {
+  rol: string | undefined;
+}
+
+
+export function Articulos({ rol }: ArticulosProps) {
   return (
-    <li className='relative flex justify-center menu'>
-      <button id='Articulos' className="flex font-semibold text-white items-center hover:text-blue-400" onClick={handleClickMenu}>
-        Artículos
-        <ButtonDow />
-      </button>
-      {
-        renderMenu.id === 'Articulos' && renderMenu.render === true && (
-          <article className="absolute -left-16 top-11 w-52">
-            <ul className=' bg-gray-900 py-2 rounded-b-lg'>
-              <li className='flex flex-col items-center w-full gap-2'>
-                <NavLink onClick={handleLinkClick} to="/bodega/stock/items"
-                  className="text-white hover:text-blue-400 font-semibold py-2 flex justify-between w-full px-6 hover:bg-slate-700">
-                  Ver Items
-                  <figure className=' text-green-600 rounded-full'>
-                    <SuccesIcon />
-                  </figure>
-                </NavLink>
-                <NavLink onClick={handleLinkClick} to="/bodega/stock/items/crearItems"
-                  className="text-white hover:text-blue-400 font-semibold py-2 flex justify-between w-full px-6 hover:bg-slate-700">
-                  Crear Items
-                  {
-                    authorize === 'Administrador' || authorize === 'Aux administrativo'
-                      ? <figure className='text-green-500 rounded-full'><SuccesIcon /></figure>
-                      : <figure className='text-red-500 rounded-full'><LockIcon /></figure>
-                  }
-                </NavLink>
-                <NavLink onClick={handleLinkClick} to="/bodega/stock/items/asignarItems"
-                  className="text-white hover:text-blue-400 font-semibold py-2 flex justify-between w-full px-6 hover:bg-slate-700">
-                  Asginar Items
-                  {
-                    authorize === 'Administrador' || authorize === 'Aux administrativo'
-                      ? <figure className='text-green-500 rounded-full'><SuccesIcon /></figure>
-                      : <figure className='text-red-500 rounded-full'><LockIcon /></figure>
-                  }
-                </NavLink>
-              </li>
-            </ul>
-          </article>
-        )
-      }
-    </li>
+      <article className="absolute -left-16 top-11 w-52">
+        <ul className=' bg-gray-900 py-2 rounded-b-lg'>
+          <li className='flex flex-col items-center w-full gap-2'>
+            <NavLink to="/bodega/stock/items"
+              className="text-white hover:text-blue-400 font-semibold py-2 flex justify-between w-full px-6 hover:bg-slate-700">
+              Ver Items
+              <figure className=' text-green-600 rounded-full'>
+                <SuccesIcon />
+              </figure>
+            </NavLink>
+            <NavLink to="/bodega/stock/items/crearItems"
+              className="text-white hover:text-blue-400 font-semibold py-2 flex justify-between w-full px-6 hover:bg-slate-700">
+              Crear Items
+              {
+                rol === 'Administrador' || rol === 'Aux administrativo'
+                  ? <figure className='text-green-500 rounded-full'><SuccesIcon /></figure>
+                  : <figure className='text-red-500 rounded-full'><LockIcon /></figure>
+              }
+            </NavLink>
+            <NavLink to="/bodega/stock/items/asignarItems"
+              className="text-white hover:text-blue-400 font-semibold py-2 flex justify-between w-full px-6 hover:bg-slate-700">
+              Asginar Items
+              {
+                rol === 'Administrador' || rol === 'Aux administrativo'
+                  ? <figure className='text-green-500 rounded-full'><SuccesIcon /></figure>
+                  : <figure className='text-red-500 rounded-full'><LockIcon /></figure>
+              }
+            </NavLink>
+          </li>
+        </ul>
+      </article>
   )
 }
