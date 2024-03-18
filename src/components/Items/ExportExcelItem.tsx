3e4dc13ed1ad12ae7a18d1b2fb741a6a377c5186
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-03-18 10:16:00 -0500

    mejorado la forma de exportar a excel el report items

--- src/components/Items/ExportExcelItem.tsx
+++ src/components/Items/ExportExcelItem.tsx
@@ -1,16 +1,17 @@
-import { ColInfo, utils, writeFile} from 'xlsx'
-import { ItemWithBodega  } from '../../interfaces/Item.Intece'
+import { type ItemWithBodega } from '../../interfaces/Item.Intece'
+import { type ColInfo, utils, writeFile } from 'xlsx'
+import { Button } from '../ui'
 
 interface RenderItemsProps {
-  items: ItemWithBodega;
+  items: ItemWithBodega
 }
 
-export const BottonExportItems = (datos: RenderItemsProps ) => {
+export const BottonExportItems = (datos: RenderItemsProps): JSX.Element => {
   const titulo = [{ A: 'Reporte de Items' }]
- 
-  const longitudes = [25, 25, 20, 10, 20, 20,]
 
-  const handleDownload = () => {
+  const longitudes = [22, 22, 20, 10, 10, 25, 10]
+
+  const handleDownload = (): void => {
     const tabla = [
       {
         A: 'NOMBRE',
@@ -23,36 +24,44 @@ export const BottonExportItems = (datos: RenderItemsProps ) => {
       }
     ]
 
-    datos.items.forEach((item) => {
-      tabla.push({
-      A: item.nombre,
-      B: item.descripcion,
-      C: item.serial,
-      D: item.placa,
-      E: item.estado,
-      F: (typeof item.bodega !== 'string' ? item.bodega?.nombre : 'No Asignado') || '',
-      G: (typeof item.bodega !== 'string' ? item.bodega?.sucursal.toString() : 'No Asignado') || ''
-      })
+    datos.items.forEach((it) => {
+      if (typeof it.bodega === 'object') {
+        tabla.push({
+          A: it.nombre,
+          B: it.descripcion,
+          C: it.serial,
+          D: it.placa,
+          E: it.estado,
+          F: it.bodega.nombre,
+          G: it.bodega.sucursal.toString()
+        })
+      } else {
+        tabla.push({
+          A: it.nombre,
+          B: it.descripcion,
+          C: it.serial,
+          D: it.placa,
+          E: it.estado,
+          F: 'No Asignado',
+          G: 'No Asignado'
+        })
+      }
     })
 
     const dataFinal = [...titulo, ...tabla]
 
     setTimeout(() => {
       creandoArchivo(dataFinal)
-    }, 1000)
+    }, 3000)
   }
 
-  const creandoArchivo = (data: unknown[]) => {
+  const creandoArchivo = (data: unknown[]): void => {
     const libro = utils.book_new()
     const hoja = utils.json_to_sheet(data, { skipHeader: true })
 
-    hoja['!merges'] = [
-      utils.decode_range('A1:G1'),
-      utils.decode_range('A2:G2'),
-      utils.decode_range('A3:G3')
-    ]
+    hoja['!merges'] = [{ s: { r: 0, c: 0 }, e: { r: 0, c: 6 } }]
 
-    const simpiedades: ColInfo[] | { width: number }[] | undefined = []
+    const simpiedades: ColInfo[] | Array<{ width: number }> | undefined = []
 
     longitudes.forEach((col) => {
       simpiedades.push({ width: col })
@@ -60,11 +69,11 @@ export const BottonExportItems = (datos: RenderItemsProps ) => {
 
     hoja['!cols'] = simpiedades
     utils.book_append_sheet(libro, hoja, 'Items')
-    writeFile(libro, 'datos.xlsx')
+    writeFile(libro, 'ReporteItems.xlsx')
   }
 
   return (
-  <button onClick={handleDownload} className='min-w-44 bg-green-800 hover:bg-green-600 text-white font-bold py-1 px-4 rounded'>
+  <Button onClick={handleDownload}>
     Exportar a Excel
-  </button>)
+  </Button>)
 }
