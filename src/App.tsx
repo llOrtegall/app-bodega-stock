3332f35ba25f57274751bb2f9630266e9a93495b
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-03-18 08:13:39 -0500

    agregado config eslint y typado fuerte

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import { ProtectedRoute } from "./components/ProtectedRoute";
-import { Routes, Route } from "react-router-dom";
-import { useAuth } from './Auth/AuthContext';
-import { useEffect } from 'react';
-import axios from "axios";
+import { ProtectedRoute } from './components/ProtectedRoute'
+import { Routes, Route } from 'react-router-dom'
+import { useAuth } from './Auth/AuthContext'
+import { useEffect } from 'react'
+import axios from 'axios'
 
-import { CrearSimcard, VerSimcards, AsignarSimcards, CreaMovimientosSim } from "./Pages/Simcards";
+import { CrearSimcard, VerSimcards, AsignarSimcards, CreaMovimientosSim } from './Pages/Simcards'
 import { CrearMovimiento, DesatalleMovimiento, ShowMovimientos } from './Pages/Movimientos'
 import { AsignarItemBodega, CrearItems, DetalleItem, VerItems } from './Pages/Items'
 import { CrearBodega, DetalleBodega, ShowBodegas } from './Pages/Bodega'
 import { LoginPage, NotFound, HomePage } from './Pages'
 
-axios.defaults.baseURL = 'http://172.20.1.70:4000/api';
+axios.defaults.baseURL = 'http://172.20.1.70:4000/api'
 // axios.defaults.baseURL = 'http://172.20.1.110:4000/api';
-export function App() {
-  const { user, login } = useAuth();
+export function App () {
+  const { user, login } = useAuth()
 
   useEffect(() => {
-    const token = localStorage.getItem('tokenBodega');
+    const token = localStorage.getItem('tokenBodega')
     if (token) {
-      login(token);
+      login(token)
     } else {
-      console.log('No Token');
+      console.log('No Token')
     }
   }, [])
- 
+
   return (
     <>
       <Routes>
@@ -40,18 +40,18 @@ export function App() {
           <Route path="/movimientos/detalle/:id" element={<DesatalleMovimiento />} />
         </Route>
 
-        <Route element={<ProtectedRoute isAllowed={!!user && user.rol === 'Administrador' || user?.rol ==='Aux Administrativo' } redirectTo="/home" />}>
+        <Route element={<ProtectedRoute isAllowed={!!user && user.rol === 'Administrador' || user?.rol === 'Aux Administrativo' } redirectTo="/home" />}>
           <Route path="/items/crearItems" element={<CrearItems />} />
           <Route path="/items/asignarItems" element={<AsignarItemBodega />} />
         </Route>
 
-        <Route element={<ProtectedRoute isAllowed={!!user && user.rol === 'Administrador' || user?.rol ==='Aux Administrativo' } redirectTo="/home" />}>
+        <Route element={<ProtectedRoute isAllowed={!!user && user.rol === 'Administrador' || user?.rol === 'Aux Administrativo' } redirectTo="/home" />}>
           <Route path="/bodega/crearBodega" element={<CrearBodega />} />
           <Route path="/bodega/crearMovimiento" element={<CrearMovimiento />} />
         </Route>
 
-        <Route element={<ProtectedRoute isAllowed={!!user && user.rol === 'Administrador' || user?.rol ==='Coordinador Soporte' } redirectTo="/home" />}>
-          <Route path="/simcards/crearSimcard"  element={<CrearSimcard />}  />
+        <Route element={<ProtectedRoute isAllowed={!!user && user.rol === 'Administrador' || user?.rol === 'Coordinador Soporte' } redirectTo="/home" />}>
+          <Route path="/simcards/crearSimcard" element={<CrearSimcard />} />
           <Route path="/simcards/asignarSimcards" element={<AsignarSimcards />} />
           <Route path="/simcards/crearMovimiento" element={<CreaMovimientosSim /> } />
         </Route>
