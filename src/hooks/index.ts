7db319f7c1570e7aeaabf43621d4abf208eb2d4f
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-03-19 08:52:40 -0500

    use Filter Mov in used

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,4 @@
 export { useFilterSimcards } from './useFilterSimcards'
 export { useFiltersBodegas } from './useFilterBodegas'
-export { useFilterMovimientos } from './useFilterMov'
 export { useFiltersItems } from './useFilterItems'
 export { useItems } from './useItems'
