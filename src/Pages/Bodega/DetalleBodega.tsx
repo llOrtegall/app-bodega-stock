560f74c448ee9300ebc27cf5782982664bb27a6d
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-03-23 11:01:29 -0500

    continuar mejorando los estilos y logica de smcards

--- src/Pages/Bodega/DetalleBodega.tsx
+++ src/Pages/Bodega/DetalleBodega.tsx
@@ -53,82 +53,84 @@ export function DetalleBodega (): JSX.Element {
         )
       }
 
-      {/* // TODO: Renderizado De Items Si Existen */}
-      <section className='mx-2 mt-2 border border-black rounded-md flex flex-col' >
-        <h3 onClick={handleToggle} className='text-center hover:underline py-2 items-center cursor-pointer'>
-          <span>{isExpanded ? '▲' : '▼'}</span> Ver Detalles Items
-        </h3>
-        {isExpanded && (bodega != null) && (
-          <>
-            <article className="flex justify-around text-center bg-blue-400 shadow-lg py-2 mb-2 text-sm">
-              <p className="font-semibold">Items</p>
-              <p className="font-semibold">Descripción</p>
-              <p className="font-semibold">Serial</p>
-              <p className="font-semibold">Placa</p>
-              <p className="font-semibold">Estado</p>
-            </article>
-            <article>
-              {
-                Array.isArray(bodega.items) && bodega.items.length !== 0
-                  ? (bodega.items.map(item => (
-                      typeof item !== 'string' && (
-                      <article key={item._id} className="grid grid-cols-5 rounded-md bg-slate-100 uppercase text-sm py-2 my-2 text-center shadow-lg">
-                        <p className="font-semibold">{item.nombre}</p>
-                        <p className="text-gray-500">{item.descripcion}</p>
-                        <p className="text-gray-700 overflow-ellipsis text-start overflow-hidden">{item.serial}</p>
-                        <p className="text-gray-500">{item.placa}</p>
-                        <p className="text-gray-500">{item.estado}</p>
-                      </article>
-                      )
-                    )))
-                  : (<p className='text-center text-2xl font-semibold'>No Existen Items Asignados</p>)
-              }
-            </article>
-          </>
-        )}
-      </section>
+      <div className='flex px-2 gap-2'>
+        {/* // TODO: Renderizado De Items Si Existen */}
+        <section className='w-1/2' >
+          <h3 onClick={handleToggle} className='text-center hover:underline py-2 items-center cursor-pointer'>
+            <span>{isExpanded ? '▲' : '▼'}</span> Ver Detalles Items
+          </h3>
+          {isExpanded && (bodega != null) && (
+            <>
+              <article className="flex justify-around text-center bg-blue-400 shadow-lg py-2 mb-2 text-sm">
+                <p className="font-semibold">Items</p>
+                <p className="font-semibold">Descripción</p>
+                <p className="font-semibold">Serial</p>
+                <p className="font-semibold">Placa</p>
+                <p className="font-semibold">Estado</p>
+              </article>
+              <article>
+                {
+                  Array.isArray(bodega.items) && bodega.items.length !== 0
+                    ? (bodega.items.map(item => (
+                        typeof item !== 'string' && (
+                        <article key={item._id} className="grid grid-cols-5 rounded-md bg-slate-100 uppercase text-sm py-2 my-2 text-center shadow-lg">
+                          <p className="font-semibold">{item.nombre}</p>
+                          <p className="text-gray-500">{item.descripcion}</p>
+                          <p className="text-gray-700 overflow-ellipsis text-start overflow-hidden">{item.serial}</p>
+                          <p className="text-gray-500">{item.placa}</p>
+                          <p className="text-gray-500">{item.estado}</p>
+                        </article>
+                        )
+                      )))
+                    : (<p className='text-center text-2xl font-semibold'>No Existen Items Asignados</p>)
+                }
+              </article>
+            </>
+          )}
+        </section>
 
-      {/* // TODO: Renderizado De Simcards Si Existen */}
-      <section className='mx-2 mt-2 border border-black rounded-md'>
-        <h3 onClick={handleToggleSim} className='text-center hover:underline py-2 items-center cursor-pointer'>
-          <span>{isExpandedSim ? '▲' : '▼'}</span> Ver Detalles Simcards
-        </h3>
+        {/* // TODO: Renderizado De Simcards Si Existen */}
+        <section className='w-1/2'>
+          <h3 onClick={handleToggleSim} className='text-center hover:underline py-2 items-center cursor-pointer'>
+            <span>{isExpandedSim ? '▲' : '▼'}</span> Ver Detalles Simcards
+          </h3>
 
-        {
-          isExpandedSim && (<>
-            <article className="grid grid-cols-7 text-center bg-blue-400 shadow-lg py-2 mb-2 text-sm">
-              <p className="font-semibold">Número</p>
-              <p className="font-semibold">Operador</p>
-              <p className="font-semibold">Estado</p>
-              <p className="font-semibold">Serial</p>
-              <p className="font-semibold">APN</p>
-              <p className="font-semibold">User</p>
-              <p className="font-semibold">Pass</p>
-            </article>
+          {
+            isExpandedSim && (<>
+              <article className="grid grid-cols-7 text-center bg-blue-400 shadow-lg py-2 mb-2 text-sm">
+                <p className="font-semibold">Número</p>
+                <p className="font-semibold">Operador</p>
+                <p className="font-semibold">Estado</p>
+                <p className="font-semibold">Serial</p>
+                <p className="font-semibold">APN</p>
+                <p className="font-semibold">User</p>
+                <p className="font-semibold">Pass</p>
+              </article>
 
-            <article className=''>
-              {
-                (bodega != null) && Array.isArray(bodega.simcards) && bodega.simcards.length !== 0
-                  ? (bodega.simcards.map(item => (
-                      typeof item !== 'string' && (
-                      <article key={item._id} className="grid grid-cols-7 rounded-md bg-slate-100 uppercase text-sm py-2 my-2 text-center shadow-lg">
-                        <p className="font-semibold">{item.numero}</p>
-                        <p className="text-gray-500">{item.operador}</p>
-                        <p className="text-gray-500">{item.estado}</p>
-                        <p className="text-gray-700 overflow-ellipsis text-start overflow-hidden">{item.serial}</p>
-                        <p className="text-gray-500">{item.apn}</p>
-                        <p className="text-gray-500">{item.user}</p>
-                        <p className="text-gray-500">{item.pass}</p>
-                      </article>
-                      )
-                    )))
-                  : (<p className='text-center text-2xl font-semibold'>No Existen Simcards Asignadas</p>)
-              }
-            </article>
-          </>
-          )
-        }
-      </section>
+              <article className=''>
+                {
+                  (bodega != null) && Array.isArray(bodega.simcards) && bodega.simcards.length !== 0
+                    ? (bodega.simcards.map(item => (
+                        typeof item !== 'string' && (
+                        <article key={item._id} className="grid grid-cols-7 rounded-md bg-slate-100 uppercase text-sm py-2 my-2 text-center shadow-lg">
+                          <p className="font-semibold">{item.numero}</p>
+                          <p className="text-gray-500">{item.operador}</p>
+                          <p className="text-gray-500">{item.estado}</p>
+                          <p className="text-gray-700 overflow-ellipsis text-start overflow-hidden">{item.serial}</p>
+                          <p className="text-gray-500">{item.apn}</p>
+                          <p className="text-gray-500">{item.user}</p>
+                          <p className="text-gray-500">{item.pass}</p>
+                        </article>
+                        )
+                      )))
+                    : (<p className='text-center text-2xl font-semibold'>No Existen Simcards Asignadas</p>)
+                }
+              </article>
+            </>
+            )
+          }
+        </section>
+      </div>
     </main>
   )
 }
