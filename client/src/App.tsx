7e4f5ba185253cabf8ac6390c027595b38a52ab2
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-03-27 08:54:17 -0500

    organizando app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,11 +10,7 @@ import { AsignarItemBodega, CrearItems, DetalleItem, VerItems } from './Pages/It
 import { CrearBodega, DetalleBodega, ShowBodegas } from './Pages/Bodega'
 import { LoginPage, NotFound, HomePage } from './Pages'
 
-// axios.defaults.baseURL = 'http://172.20.1.70:4000/api'
-// axios.defaults.baseURL = 'http://172.20.1.110:4000/api'
-// axios.defaults.baseURL = 'http://192.168.0.105:4000/api'
-
-axios.defaults.baseURL = 'http://api:4000/api'
+axios.defaults.baseURL = 'http://localhost:4000/api'
 
 export function App (): JSX.Element {
   const { user, login } = useAuth()
