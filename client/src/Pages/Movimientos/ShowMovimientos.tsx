a7ade140a013c2462b479f7f69107752715f659a
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-04-01 12:37:21 -0500

    ver movimiento ok dark and light

--- client/src/Pages/Movimientos/ShowMovimientos.tsx
+++ client/src/Pages/Movimientos/ShowMovimientos.tsx
@@ -14,12 +14,12 @@ export function ShowMovimientos(): JSX.Element {
       ? (<section className='flex justify-center pt-20'><Loading>Cargando Movimientos</Loading></section>)
       : (
         <section className="">
-          <article className='flex items-center justify-center py-2 gap-2'>
+          <article className='flex items-center justify-center py-2 gap-2 bg-blue-300 dark:bg-blue-950'>
             <Label textColor='text-black dark:text-white'>Filtrar Por: </Label>
             <Input type="text" value={busMov} onChange={ev => { setBusMov(ev.target.value) }} placeholder="N° Mov | N° Incidente" />
           </article >
           <section className=''>
-            <article className='grid grid-cols-12 px-2 text-center p-2 font-semibold bg-blue-200 text-xs'>
+            <article className='grid grid-cols-12 px-2 text-center p-2 font-semibold bg-blue-200 dark:bg-blue-700 dark:text-white text-xs 2xl:text-lg'>
               <p className='col-span-1 cursor-pointer hover:underline' 
                 onClick={toggleSortOrder}> N° Mov <span>{sortOrder === 'asc' ? '▼' : '▲'}</span></p>
               <p className='col-span-2'> Fecha Mov</p>
