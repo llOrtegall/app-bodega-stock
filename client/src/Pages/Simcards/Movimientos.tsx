311e72497fe00cfdad57766468c1f9e2f6e6665c
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-04-04 12:42:18 -0500

    vamoe mejorando el componente movSimcards

--- client/src/Pages/Simcards/Movimientos.tsx
+++ client/src/Pages/Simcards/Movimientos.tsx
@@ -1,148 +1,50 @@
-import { RenderBodegaDestino } from '../../components/simcards/RenderBodegaDestino.js'
-import { RenderBodegaOrigen } from '../../components/simcards/RenderBodegaOrigen.js'
-import { ComponenteSimcards } from '../../components/simcards/ComponenteSimcards.js'
-import { FooterMovSim } from '../../components/simcards/FooterCompSimcaMov.js'
-import { MessageDisplay } from '../../components/ui/MessagesDisplay.js'
-import { type BodegaWithSims } from '../../types/Simcard.interfaces.js'
-import { useAuth } from '../../Auth/AuthContext'
-import { useCallback, useState } from 'react'
+import { RenderBodega } from '../../components/simcards/RenderBodegaOrigen'
+import { BodegaWithSims } from '../../types/Simcard.interfaces'
+import { useState } from 'react'
 import axios from 'axios'
 
-interface CartSims {
-  cartSims: string[]
-  handleAddSimcard: (id: string) => void
-  handleRemoveItem: (id: string) => void
-  setCartSims: (value: string[]) => void
-}
+export function CreaMovimientosSim(): JSX.Element {
 
-interface CartSims2 {
-  cartSims2: string[]
-  handleAddSimcard2: (id: string) => void
-  handleRemoveItem2: (id: string) => void
-  setCartSims2: (value: string[]) => void
-}
+  // const handleClick = (): void => {
+  //   if (bodegaOrigen === bodegaDestino) {
+  //     setTimeout(() => {
+  //       setMessage('')
+  //       setError('')
+  //     }, 4000)
+  //     setError('Debe Ingresar Una Bodega De Origen y Una De Destino Diferentes !!!'); return
+  //   }
+  //   axios.post('/moveSimcard', {
+  //     bodegas: { bodegaOrigen: bodegaOrigen._id, bodegaDestino: bodegaDestino._id },
+  //     simsIds: { entran: cartSims, salen: cartSims2 },
+  //     encargado: nombres,
+  //     descripcion,
+  //     incidente,
+  //     company
+  //   })
+  //     .then(res => {
+  //       setMessage(res.data.message as string)
+  //       setBodegaOrigen({ _id: '', nombre: '', direccion: '', sucursal: 0, items: [], simcards: [], updatedAt: '' })
+  //       setBodegaDestino({ _id: '', nombre: '', direccion: '', sucursal: 0, items: [], simcards: [], updatedAt: '' })
+  //       setCartSims([]); setCartSims2([]); setDescripcion(''); setIncidente(''); setTimeout(() => { setMessage(''); setError('') }, 4000)
+  //     })
+  //     .catch(err => {
+  //       setError(err.response.data.error as string)
+  //       setTimeout(() => { setMessage(''); setError('') }, 4000)
+  //     })
+  // }
 
-function useCarSimcards (initialItems = [] as string[]): CartSims {
-  const [cartSims, setCartSims] = useState(initialItems)
-
-  const handleAddSimcard = useCallback((id: string) => {
-    setCartSims(prevItems => {
-      if (!prevItems.includes(id)) {
-        return [...prevItems, id]
-      } else {
-        return prevItems
-      }
-    })
-  }, [])
-
-  const handleRemoveItem = useCallback((id: string) => {
-    setCartSims(prevItems => {
-      return prevItems.filter(item => item !== id)
-    })
-  }, [])
-
-  return { cartSims, handleAddSimcard, handleRemoveItem, setCartSims }
-}
-
-function useCarSimcards2 (initialItems = [] as string[]): CartSims2 {
-  const [cartSims2, setCartSims2] = useState(initialItems)
-
-  const handleAddSimcard2 = useCallback((id: string) => {
-    setCartSims2(prevItems => {
-      if (!prevItems.includes(id)) {
-        return [...prevItems, id]
-      } else {
-        return prevItems
-      }
-    })
-  }, [])
-
-  const handleRemoveItem2 = useCallback((id: string) => {
-    setCartSims2(prevItems => {
-      return prevItems.filter(item => item !== id)
-    })
-  }, [])
-
-  return { cartSims2, handleAddSimcard2, handleRemoveItem2, setCartSims2 }
-}
-
-export function CreaMovimientosSim (): JSX.Element {
-  const { user } = useAuth()
-  const company = user.empresa
   const [bodegaOrigen, setBodegaOrigen] = useState<BodegaWithSims>({ _id: '', nombre: '', direccion: '', sucursal: 0, items: [], simcards: [], updatedAt: '' })
   const [bodegaDestino, setBodegaDestino] = useState<BodegaWithSims>({ _id: '', nombre: '', direccion: '', sucursal: 0, items: [], simcards: [], updatedAt: '' })
 
-  const [descripcion, setDescripcion] = useState('')
-  const [incidente, setIncidente] = useState('')
-
-  const [message, setMessage] = useState('')
-  const [error, setError] = useState('')
-
-  const { cartSims, setCartSims, handleAddSimcard, handleRemoveItem } = useCarSimcards()
-  const { cartSims2, setCartSims2, handleAddSimcard2, handleRemoveItem2 } = useCarSimcards2()
-
-  const hadlesearchnew = (): void => {
-    setCartSims([])
-    setCartSims2([])
+  const getBodega = async ({ company, sucursal }: { sucursal: string, company: string }): Promise<BodegaWithSims> => {
+    const response = await axios.get(`/getBodegaSimcards/${company}/${sucursal}`)
+    return response.data as BodegaWithSims
   }
-
-  const handleClick = (): void => {
-    if (bodegaOrigen === bodegaDestino) {
-      setTimeout(() => {
-        setMessage('')
-        setError('')
-      }, 4000)
-      setError('Debe Ingresar Una Bodega De Origen y Una De Destino Diferentes !!!'); return
-    }
-    axios.post('/moveSimcard', {
-      bodegas: { bodegaOrigen: bodegaOrigen._id, bodegaDestino: bodegaDestino._id },
-      simsIds: { entran: cartSims, salen: cartSims2 },
-      encargado: nombres,
-      descripcion,
-      incidente,
-      company
-    })
-      .then(res => {
-        setMessage(res.data.message as string)
-        setBodegaOrigen({ _id: '', nombre: '', direccion: '', sucursal: 0, items: [], simcards: [], updatedAt: '' })
-        setBodegaDestino({ _id: '', nombre: '', direccion: '', sucursal: 0, items: [], simcards: [], updatedAt: '' })
-        setCartSims([]); setCartSims2([]); setDescripcion(''); setIncidente(''); setTimeout(() => { setMessage(''); setError('') }, 4000)
-      })
-      .catch(err => {
-        setError(err.response.data.error as string)
-        setTimeout(() => { setMessage(''); setError('') }, 4000)
-      })
-  }
-
-  const nombres = user?.nombres + ' ' + user?.apellidos
-
+ 
   return (
-    <main className="w-full min-h-[92vh] ">
-
-      <section className="grid grid-cols-4 p-2 gap-2">
-
-        <RenderBodegaOrigen bodegaOrigen={bodegaOrigen} setBodegaOrigen={setBodegaOrigen}
-          cartSims={cartSims} handleAddSimcard={handleAddSimcard} fun={hadlesearchnew} company={company} />
-
-        <RenderBodegaDestino bodegaDestino={bodegaDestino} setBodegaDestino={setBodegaDestino}
-          cartSims2={cartSims2} handleAddSimcard2={handleAddSimcard2} fun={hadlesearchnew} company={company} />
-      </section>
-
-      <article className='mx-2 rounded-md'>
-
-        <ComponenteSimcards bodegaOrigen={bodegaOrigen} bodegaDestino={bodegaDestino} cartSims={cartSims}
-          cartSims2={cartSims2} handleRemoveItem={handleRemoveItem} handleRemoveItem2={handleRemoveItem2} />
-      </article>
-
-      <section>
-
-        <FooterMovSim encargado={nombres} incidente={incidente} setIncidente={setIncidente}
-          descripcion={descripcion} setDescripcion={setDescripcion} handleClick={handleClick} />
-      </section>
-
-      <section className='pt-4 flex items-center justify-center'>
-        <MessageDisplay message={message} error={error} />
-      </section>
+    <main className="w-full min-h-[92vh] flex gap-2 mx-2 mt-2">
+      <RenderBodega fun={getBodega} sendBodega={setBodegaOrigen} renderInfo={bodegaOrigen}/>
+      <RenderBodega fun={getBodega} sendBodega={setBodegaDestino} renderInfo={bodegaDestino}/>
     </main>
   )
 }
--- client/src/components/simcards/RenderBodegaOrigen.tsx
+++ client/src/components/simcards/RenderBodegaOrigen.tsx
@@ -1,73 +1,39 @@
-import { type BodegaWithSims } from '../../types/Simcard.interfaces'
-import { useFilterSimcards } from '../../hooks'
-import { AddIcon } from '../icons'
-import { useState } from 'react'
-import axios from 'axios'
+import { useState } from "react"
+import { useAuth } from "../../Auth/AuthContext"
+import { BodegaWithSims } from "../../types/Simcard.interfaces"
+import { Button, Input, Label } from "../ui"
 
-interface RenderBodegaOrigenProps {
-  bodegaOrigen: BodegaWithSims
-  setBodegaOrigen: React.Dispatch<React.SetStateAction<BodegaWithSims>>
-  cartSims: string[]
-  handleAddSimcard: (id: string) => void
-  fun: () => void
-  company: string
+interface Props {
+  fun: ({ company, sucursal }: { sucursal: string, company: string }) => Promise<BodegaWithSims>
+  sendBodega: (bodega: BodegaWithSims) => void
+  renderInfo: BodegaWithSims
 }
 
-export function RenderBodegaOrigen ({ bodegaOrigen, setBodegaOrigen, cartSims, handleAddSimcard, fun, company }: RenderBodegaOrigenProps): JSX.Element {
-  const [searchBodegaOrigen, setSearchBodegaOrigen] = useState('')
+export function RenderBodega({ fun, sendBodega, renderInfo }: Props): JSX.Element {
+  const [sucursal, setSucursal] = useState('')
 
-  const hadlesearchnew = fun
+  const { user } = useAuth()
+  const company = user.empresa
 
-  const searchOrigen = (ev: { preventDefault: () => void }): void => {
+  const handleSubmit = (ev: { preventDefault: () => void }) => {
     ev.preventDefault()
-
-    if (bodegaOrigen !== null) {
-      hadlesearchnew()
-      setBodegaOrigen({ _id: '', nombre: '', direccion: '', sucursal: 0, items: [], simcards: [], updatedAt: '' })
-      axios.get(`/getBodegaSimcards/${company}/${searchBodegaOrigen}`)
-        .then(response => {
-          setBodegaOrigen(response.data as BodegaWithSims)
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    } else {
-      axios.get(`/getBodegaSimcards/${company}/${searchBodegaOrigen}`)
-        .then(response => {
-          setBodegaOrigen(response.data as BodegaWithSims)
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    }
+    fun({ company, sucursal })
+      .then(res => sendBodega(res))
   }
 
-  const simCards = bodegaOrigen.simcards
-  const { filteredSimcards, searchSimcard, setSearchSimcard } = useFilterSimcards(simCards)
-
   return (
-    <article className="col-span-2 text-md flex flex-col gap-2">
-
-      <form className="w-full p-1 bg-gray-600 rounded-lg flex items-center gap-2 text-center col-span-2 place-content-center"
-        onSubmit={searchOrigen}>
-        <h3 className="font-semibold text-white">Buscar Bodega Origen</h3>
-        <input type="text" value={searchBodegaOrigen} onChange={ev => { setSearchBodegaOrigen(ev.target.value) }}
-          placeholder="40001 | 34545"
-          className="bg-slate-100 w-64 p-1 rounded-md " />
-        <button className="bg-green-600 text-white rounded-md p-1 font-semibold hover:bg-white hover:text-black">Buscar Sucursal</button>
+    <article className="w-full flex flex-col gap-2">
+      <form className="w-full p-1 bg-blue-300 flex items-center gap-2 text-center col-span-2 place-content-center" onSubmit={handleSubmit}>
+        <Label>Buscar Bodega Origen</Label>
+        <Input type="text" placeholder="40001 | 34545" value={sucursal} onChange={(ev) => setSucursal(ev.target.value)} />
+        <div><Button>Buscar Sucursal</Button></div>
       </form>
 
-      <header className="w-full flex justify-around rounded-md p-2 bg-slate-600 text-white place-items-center">
-        <h3> <span className="font-bold">Nombre:</span>  {bodegaOrigen?.nombre}</h3>
-        <p> <span className="font-bold">Direccion:</span>  {bodegaOrigen?.direccion}</p>
-        <p> <span className="font-bold">Sucursal:</span>  {bodegaOrigen?.sucursal}</p>
+      <header className="w-full p-1 bg-blue-300 flex items-center gap-4 text-center col-span-2 place-content-center">
+        <p> <span className="font-semibold">Nombre: </span> {renderInfo.nombre} </p>
+        <p> <span className="font-semibold">Direccion: </span> {renderInfo.direccion}</p>
+        <p> <span className="font-semibold">Sucursal: </span> {renderInfo.sucursal}</p>
       </header>
-
-      <section className="grid grid-cols-2 w-full place-items-center gap-6 bg-slate-600 text-white rounded-md px-4 py-2">
-        <p><span className="font-semibold pr-2">Filtrar:</span>| Número | Serial | Operador |</p>
-        <input type="text" placeholder="Buscar Items..." className="bg-slate-100 w-64 rounded-md p-1 text-black"
-          value={searchSimcard} onChange={ev => { setSearchSimcard(ev.target.value) }} />
-      </section>
 
       <section className="grid grid-cols-4 w-full place-items-center p-2 bg-slate-600 rounded-md  text-white">
         <p className="font-semibold">Número</p>
@@ -76,25 +42,6 @@ export function RenderBodegaOrigen ({ bodegaOrigen, setBodegaOrigen, cartSims, h
         <p className="font-semibold">Agregar</p>
       </section>
 
-      <section style={{ maxHeight: '330px', overflowY: 'auto' }} className=''>
-        {
-          (
-            filteredSimcards.map(item => (
-              <section key={item._id} className="w-full grid grid-cols-4 p-2 bg-blue-500 rounded-md mb-2 place-items-center text-white transition-colors hover:text-black hover:bg-slate-200 cursor-default">
-                <p>{item.numero}</p>
-                <p>{item.operador}</p>
-                <p>{item.serial}</p>
-                <button
-                  onClick={() => { handleAddSimcard(item._id) }}
-                  className={cartSims.includes(item._id) ? 'added' : 'rounded-full transition-colors hover:bg-green-300  hover:text-black'}
-                >
-                  <AddIcon />
-                </button>
-              </section>
-            ))
-          )
-        }
-      </section>
 
     </article>
   )
