3f5e375d511f03e5599893a418b9950782c5c06b
llOrtegall <IvanOrtega_97@hotmail.com>
2024-04-26 15:12:05 -0500

    antes de agregar el eslint

--- api_v1/src/index.ts
+++ api_v1/src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 const port = 9040;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World !!!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
