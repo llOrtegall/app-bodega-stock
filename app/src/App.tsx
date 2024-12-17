d8230eb4e503557767a0bf5bb71cbc3a76d79005
llOrtegall <IvanOrtega_97@hotmail.com>
2024-12-17 09:30:56 -0500

    Initialize React app with TypeScript, Vite, and Tailwind CSS setup

---
+++ app/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1>React App</h1>
+      <p>Start editing to see some </p>
+
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
