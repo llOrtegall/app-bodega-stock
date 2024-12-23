769a029eefc611e7d761a102fdb057a70235427b
llOrtegall <IvanOrtega_97@hotmail.com>
2024-12-23 09:32:12 -0500

    Refactor MainLayout component for consistent import style and code cleanup

--- app/src/Layouts/MainLayout.tsx
+++ app/src/Layouts/MainLayout.tsx
@@ -1,20 +1,14 @@
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
-import { Toaster } from "@/components/ui/sonner"
-import { Outlet } from "react-router"
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import { AppSidebar } from '@/components/app-sidebar'
+import { Toaster } from '@/components/ui/sonner'
+import { Outlet } from 'react-router'
 
 export default function MainLayout() {
   return (
-    <section className="w-full h-screen">
+    <section className='w-full h-screen'>
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-          {/* <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-            </div>
-          </header> */}
           <Outlet />
         </SidebarInset>
       </SidebarProvider>
